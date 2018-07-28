233587f3f37568e40daf6f108ec616bfe1985b2a
joecosta117 <joecosta117@gmail.com>
2018-07-28 16:08:45 -0400

    testing git gutter

--- test.js
+++ test.js
@@ -1,3 +1,12 @@
-function hello(){
+function hello() {
 	return "Hello World";
 }
+
+function example(obj, num) {
+	if (typeof num !== 'numer') throw Error('oops');
+	return obj["age"] === num;
+	hey
+
+}
+
+console.log(hello())
